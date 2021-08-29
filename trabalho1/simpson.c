07461c7974b5717e96bb89b7e0018f4113fd9a09
Igor de Araujo Vilhalba <igor.vilhalba@gmail.com>
2021-08-29 17:11:38 -0300

    Adiciona calc_func()

--- trabalho1/simpson.c
+++ trabalho1/simpson.c
@@ -8,13 +8,34 @@ struct simpson_args {
 	int nthreads; 	// quantidade de threads
 };
 
+/* Calcula y a partir de func (uma função de x), a struct de variáveis associadas a func,
+ * os limites de integração e quantidade de subintervalos
+ * Retorna um vetor dinamicamente alocado */
+double *calc_func(struct expr *func, struct expr_var_list *vars, double a, double b, int n) {
+	double *y = (double *)malloc(sizeof(double) * (n + 1));
+	double step = (b - a) / n;
+	/* obtem o endereco de x */
+	struct expr_var *x = expr_var(vars, "x", 1);
+	x->value = a;
+	for (int i = 0; i <= n; i++) {
+		y[i] = expr_eval(func);
+		x->value += step;
+	}
+	return y;
+}
+
 /* Função que calcula a integral com um número variável de threads
  * utilizando o método de Simpson
  * Args:
  * 	- func: função compilada
+ * 	- vars: variáveis associadas a `func`
  * 	- a, b: intervalo de integração
  * 	- n: 	quantidade de subintervalos
  * 	- nthreads: quantidade de threads
  */
-double simpson_method(struct expr func, int a, int b, int n, int nthreads) {
+double simpson_method(struct expr *func, struct expr_var_list *vars, double a, double b, int n, int nthreads) {
+	double resultado;
+	pthread_t threads[nthreads];
+	double *y;
+	return resultado;
 }
