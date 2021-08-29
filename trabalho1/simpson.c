52eb7bf28b672630c073f351047314463a3fb78e
ciafrino <chrisciafrino@ufrj.br>
2021-08-29 18:43:31 -0300

    Add simpson sequencial

--- trabalho1/simpson.c
+++ trabalho1/simpson.c
@@ -13,18 +13,18 @@ struct simpson_args {
  * Retorna um vetor dinamicamente alocado */
 double *calc_func(struct expr *func, struct expr_var_list *vars, double a, double b, int n) {
 	double *y = (double *)malloc(sizeof(double) * (n + 1));
-	double step = (b - a) / n;
+	double step = (b - a) / n / 2.0;
 	/* obtem o endereco de x */
 	struct expr_var *x = expr_var(vars, "x", 1);
 	x->value = a;
-	for (int i = 0; i <= n; i++) {
+	for (int i = 0; i < (n << 1); i++) {
 		y[i] = expr_eval(func);
 		x->value += step;
 	}
 	return y;
 }
 
-/* Função que calcula a integral com um número variável de threads
+/* Função que aproxima a integral com um número variável de threads
  * utilizando o método de Simpson
  * Args:
  * 	- func: função compilada
@@ -38,4 +38,41 @@ double simpson_method(struct expr *func, struct expr_var_list *vars, double a, d
 	pthread_t threads[nthreads];
 	double *y;
 	return resultado;
+}
+
+/* Função que aproxima a integral
+ * utilizando o método de Simpson
+ * Args:
+ * 	- func: função compilada
+ * 	- vars: variáveis associadas a `func`
+ * 	- a, b: intervalo de integração
+ * 	- n: 	quantidade de subintervalos
+ */
+double sequential_simpson_method(struct expr *func, struct expr_var_list *vars, double a, double b, int n) {
+	double resultado;
+
+	// tamanho do subintervalo
+	double step = (b - a) / n / 2.0;
+
+	/* obtem o endereco de x */
+	struct expr_var *x = expr_var(vars, "x", 1);
+
+	x->value = b;
+	double end = expr_eval(func);
+	
+	x->value = a;
+	double start = expr_eval(func);
+
+	resultado = start + end;
+
+	for (int i = 1; i < (n << 1); ++i) {
+		if (i & 1 == 1) {
+			resultado += expr_eval(func) * 4.0;
+		} else { // termo par tem peso 2
+			resultado += expr_eval(func) * 2.0;
+		}
+		x->value += step;
+	}
+
+	return resultado / 3.0 * step;
 }
