32d33e11dfdcd7bcde5e009610e55cd43c53d297
Igor de Araujo Vilhalba <igor.vilhalba@gmail.com>
2021-10-11 22:53:14 -0300

    Atualiza executa.sh

--- trabalho2/testes/executa.sh
+++ trabalho2/testes/executa.sh
@@ -1,7 +1,7 @@
 while IFS=" " read -ra infos; do
 	sufix="${infos[0]}"
 	N="${infos[1]}"
-	for C in {1..6}; do
+	for C in {1..4}; do
 		for reps in {1..5}; do
 			../main "entradas/entrada${sufix}.txt" "saidas/saida${sufix}.txt" "$N" "$C"
 		done
